4098462af5bcf25ac335d48d829668869108fdb3
Lucas Alves da Silva <web4dev.lucas@outlook.com>
2022-11-20 21:08:54 -0300

    inicial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ title }}</h1>
+    <div>
+      <input
+        type="text"
+        size="25"
+        placeholder="Digite para gerar..."
+        v-model="text"
+      />
+    </div>
+
+    <div v-if="text" class="output">
+      <img :src="newQRCode" alt="QRCode" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import QRious from "qrious";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      title: "Gerador QRCode",
+      text: "https://www.google.com.br",
+      qrcode: new QRious({ size: 300 }),
+    };
+  },
+  computed: {
+    newQRCode() {
+      this.qrcode.value = this.text;
+      return this.qrcode.toDataURL();
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@font-face {
+  font-family: "RobotoMono";
+  src: url("./assets/fonts/RobotoMono-Thin.ttf");
+}
+
+* {
+  font-family: "RobotoMono", monospace;
+}
+
+body {
+  display: flex;
+  justify-content: center;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fff;
+  background: linear-gradient(to right, rgb(19, 106, 138), rgb(38, 120, 113));
+}
+
+input {
+  font-size: 2em;
+  margin-bottom: 30px;
+}
+
+.output {
+  border: 1px solid #222;
+  padding: 30px 15px;
+  background-color: #0006;
 }
 </style>
